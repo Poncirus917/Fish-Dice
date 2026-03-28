1931ab88603c98aa806f4e940135e44fd83a3a7f
Poncirus917 <nicola20040917@163.com>
2026-03-28 20:35:13 +0800

    fix: enable export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 关键：强制 Next.js 生成静态 HTML 文件
+  images: {
+    unoptimized: true, // 静态导出必须禁用图片优化
+  },
 };
 
 export default nextConfig;
